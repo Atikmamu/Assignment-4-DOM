15fec5b86f8717e8c14aec9e383370c560b0f877
Atikmamu <robinatik50@gmail.com>
2024-09-26 17:44:06 +0600

    added equation and mobile responsive all section done

--- js/donateNow.js
+++ js/donateNow.js
@@ -8,22 +8,34 @@ document.getElementById("btn-donate-now")
     const donateNow = getInputFieldValueById("input-donate-now");
 
        // number na hoi tahola donate hobe na..  > validation 
-       if(isNaN(donateNow)){
-          alert('Failed to add money');
+       if(donateNow <= 0 || isNaN(donateNow)){
+          alert('Invalid Donation amount');
           return;
        }
 
     const balance = getTextFieldValueById("donate-balance");
+    const mainBalance = getTextFieldValueById('main-balance');
+
+    if(donateNow > mainBalance){
+      alert('you do not have enough money to donate');
+      return;
+    }
 
     const newBalance = balance + donateNow;
 
     document.getElementById("donate-balance").innerText = newBalance;
+
+    const newBalance2 = mainBalance - donateNow;
+
+    document.getElementById('main-balance').innerText = newBalance2;
+
+    document.getElementById('my_modal_1').showModal()
     
     const div = document.createElement('div');
     const newDate = new Date().toString();
     const newElement = document.getElementById('transaction-container')
-    div.innerHTML= `<div class="border"><p class="text-gray-600"><span class="text-2xl font-bold">${donateNow} Taka is Donated for Donate for Flood at Noakhali, Bangladesh</span>;
-    </p><p class="text-md text-gray-500 p-4 rounded-lg">${newDate}</p></div>`
+    div.innerHTML= `<div class="border py-4 pl-6 rounded-lg"><p class="text-black"><span class="text-xl font-bold">${donateNow} Taka is Donated for Donate for Flood at Noakhali, Bangladesh</span>;
+    </p><p class="text-md text-black p-4 rounded-lg">Date:${newDate}</p></div>`
 
     newElement.appendChild(div);
   });
@@ -37,25 +49,35 @@ document.getElementById("btn-feni-donate")
 
     const feniDonate = getInputFieldValueById("input-donate-feni");
 
-
-     // number na hoi tahola donate hobe na..  > validation 
-     if(isNaN(feniDonate)){
-      alert('Failed to add money');
+     if(feniDonate <= 0 || isNaN(feniDonate)){
+      alert('Invalid Donation amount');
       return;
    }
 
+   const balance2 = getTextFieldValueById("feni-donate-balance");
+    const mainBalance = getTextFieldValueById('main-balance');
 
-    const balance2 = getTextFieldValueById("feni-donate-balance");
+    if(feniDonate > mainBalance){
+      alert('you do not have enough money to donate');
+      return;
+    }
+    
     const newBalance2 = balance2 + feniDonate;
 
     document.getElementById("feni-donate-balance").innerText = newBalance2;
+
+    const newBalance3 = mainBalance - feniDonate;
+
+    document.getElementById('main-balance').innerText = newBalance3;
+
+    document.getElementById('my_modal_2').showModal()
 
 
     const div = document.createElement('div');
     const newDate = new Date().toString();
     const newElement = document.getElementById('transaction-container')
-    div.innerHTML= `<div class="border"><p class="text-gray-600"><span class="text-2xl font-bold">${feniDonate} Taka is Donated for Donate for Flood at Feni, Bangladesh</span>;
-    </p><p class="text-md text-gray-500 p-4 rounded-lg">${newDate}</p></div>`
+    div.innerHTML= `<div class="border py-4 pl-6 rounded-lg"><p class="text-black"><span class="text-xl font-bold">${feniDonate} Taka is Donated for Donate for Flood at Feni, Bangladesh</span>;
+    </p><p class="text-md p-4 rounded-lg text-black">Date:${newDate}</p></div>`
 
     newElement.appendChild(div);
   });
@@ -72,22 +94,36 @@ document.getElementById("btn-feni-donate")
 
 
        // number na hoi tahola donate hobe na..  > validation 
-     if(isNaN(quotaDonate)){
-      alert('Failed to add money');
+     if(quotaDonate <= 0 || isNaN(quotaDonate)){
+      alert('Invalid Donation amount');
       return;
    }
 
       const balance3 = getTextFieldValueById("quota-donate-balance");
-      const newBalance3 = balance3 + quotaDonate;
+      const mainBalance = getTextFieldValueById('main-balance');
 
-      document.getElementById("quota-donate-balance").innerText = newBalance3;
+      if(quotaDonate > mainBalance){
+        alert('you do not have enough money to donate');
+        return;
+      }
+
+       
+    const newBalance3 = balance3 + quotaDonate;
+
+    document.getElementById("quota-donate-balance").innerText = newBalance3;
+
+    const newBalance4 = mainBalance - quotaDonate;
+
+    document.getElementById('main-balance').innerText = newBalance4;
+
+    document.getElementById('my_modal_3').showModal()
 
 
       const div = document.createElement('div');
     const newDate = new Date().toString();
     const newElement = document.getElementById('transaction-container')
-    div.innerHTML= `<div class="border"><p class="text-gray-600"><span class="text-2xl font-bold">${quotaDonate} Taka is Donated for Donate Aid for Injured in the Quota Movement</span>;
-    </p><p class="text-md text-gray-500 p-4 rounded-lg">${newDate}</p></div>`
+    div.innerHTML= `<div class="border py-4 pl-6 rounded-lg"><p class="text-black"><span class="text-xl font-bold">${quotaDonate} Taka is Donated for Donate Aid for Injured in the Quota Movement</span>
+    </p><p class="text-md text-black p-4 rounded-lg">Date:${newDate}</p></div>`
 
     newElement.appendChild(div);
     });
